63ec9a7e5aa41b8d9d567311124dad51468723dd
notna24 <debian-user@debian.debian>
2022-03-07 00:18:23 +0100

    saving progress

--- analyzer.py
+++ analyzer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from configparser import RawConfigParser
 import time
 import sqlite3
 import matplotlib as mpl
@@ -7,22 +8,29 @@ from sys import platform
 
 
 def find_least_visit_times(weeks):
-    pass
+	pass
 
-def plot_timeinterval():
-    pass
+def plot_timeinterval(path, start, end, bibs):
+	#this function plots a the spaces in a certain time interval
+	data = {}
+	for bib in bibs:
+		raw_data = get_data(path, start, end, bib)
+		data[bib] = [i for i in raw_data[0]]
+		
+
+	
 
 
-def get_data(path, start, end) -> list:
-    with sqlite3.connect(path) as con:
-        cur = con.cursor()
-        cur.execute("SELECT * FROM bibs WHERE date BETWEEN ? AND ?",
+def get_data(path, start, end, bib) -> list:
+	with sqlite3.connect(path) as con:
+		cur = con.cursor()
+		cur.execute("SELECT * FROM bibs WHERE date BETWEEN ? AND ?",
 		(start, end)
 	)
-        print(cur.fetchall())
+	print(cur.fetchall())
 
 
 if __name__ == "__main__":
-    #db_path = "/var/lib/bib-spaces-analyzer/spaces.db" if platform.startswith("linux") else "spaces.db"
-    db_path = input("path to database")
-    get_data(db_path)
+	#db_path = "/var/lib/bib-spaces-analyzer/spaces.db" if platform.startswith("linux") else "spaces.db"
+	#db_path = input("path to database")
+	get_data(db_path)
